97e6116a83f209d7865645da91122284d72acdc1
Mike <josemimore0@gmail.com>
2023-02-28 12:21:24 -0400

    set up project

---
+++ code.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('container')
+
+const harryPotter = {
+    'title': 'Harry Potter',
+    'author': 'Elena',
+    'pages': '265',
+    'read': true,
+}
+
+let library = [harryPotter, harryPotter];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function displayCard(card) {
+    document.createElement
+}
+
+function addBookToLibrary() {
+
+}
+
+displayCard(harryPotter)
