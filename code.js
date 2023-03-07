6a269ac6c5f2c9e3c44c2063cc0e03b4de3ffcf7
Mike <josemimore0@gmail.com>
2023-03-07 13:38:18 -0400

    Added modal

--- code.js
+++ code.js
@@ -1,14 +1,7 @@
 const container = document.getElementById('container');
-const btnAddBook = document.getElementById('btn-add-book')
-
-const harryPotter = {
-    'title': 'Harry Potter',
-    'author': 'Elena',
-    'pages': '265',
-    'read': true,
-}
-
-let library = [harryPotter, harryPotter];
+const btnAddBook = document.getElementById('addBookBtn')
+const modal = document.getElementById('addBookModal');
+const overlay = document.getElementById('overlay');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -17,21 +10,32 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function createCard() {
+const harryPotter = new Book('Harry potter', 'elena', 200, true);
+const BlackList = new Book('Black List', 'Ben', 150, false)
+
+let library = [];
+
+function addBookForm() {
+    modal.classList.add('active');
+    overlay.classList.add('active')
+}
+
+function createCard() {  
     let newCard = document.createElement('div');
     newCard.classList.add('card');
-    newCard.innerHTML = `
-    <div class="card">
-        <h3>${harryPotter.title}</h3>
-        <hr>
-        <h3>${harryPotter.author}</h3>
-        <hr>
-        <h3>${harryPotter.pages}</h3>
-        <hr>
-        <h3>${harryPotter.read}</h3>
-        <hr>
-        <button class="btn-remove">Remove</button>
-    </div>`
+
+    let titleH3 = document.createElement('h3');
+    let authorH3 = document.createElement('h3');
+    let pagesH3 = document.createElement('h3');
+    let readH3 = document.createElement('h3');
+
+    titleH3.textContent = harryPotter.title;
+    authorH3.textContent = harryPotter.author;
+    pagesH3.textContent = harryPotter.pages;
+    readH3.textContent = harryPotter.read;
+
+    newCard.append(titleH3, authorH3, pagesH3, readH3)
+
     container.append(newCard);
 }
 
@@ -39,4 +43,5 @@ function addBookToLibrary() {
 
 }
 
-btnAddBook.addEventListener('click', createCard)
+
+btnAddBook.addEventListener('click', addBookForm);
